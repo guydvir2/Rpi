156eb55d67232fb74af3297379b78be083a4eb6e
guydvir <guydvir.tech@gmail.com>
2018-03-01 15:09:33 +0200

    phhhh

--- GPIO_Projects/PIR2.py
+++ GPIO_Projects/PIR2.py
@@ -1,49 +1,38 @@
 from gpiozero import MotionSensor
-#from gpiozero import Buzzer
+# from gpiozero import Buzzer
 from gpiozero import LED
 from time import sleep
-import  Adafruit_DHT
+import Adafruit_DHT
 import lcddriver
 import picamera
 
-
-
-
-
-def center_str(text_in):
-    text_out=" "*round((16-len(text_in))/2)+text_in
-    return text_out
-
-display = lcddriver.lcd()
+#
+#
+# def center_str(text_in):
+#     text_out=" "*round((16-len(text_in))/2)+text_in
+#     return text_out
+#
+# display = lcddriver.lcd()
 pir = MotionSensor(4)
-led_red = LED (17)
+led_red = LED(17)
 camera = picamera.PiCamera()
 
-#buz = Buzzer (17)
-#buz
 
-display.lcd_clear()
-display.lcd_display_string(center_str("Hello"), 1)
-sleep (2)
-display.lcd_clear()
-
-
-
-x=0
+x = 0
 while True:
     while pir.wait_for_motion():
-        x +=1
-        print("Alarm #",x)
+        x += 1
+        print("Alarm #", x)
         display.lcd_clear()
         display.lcd_display_string(center_str("Shachar"), 1)
-        display.lcd_display_string("Alarm #"+str(x), 2)
-        camera.capture('/home/guy/image%d.jpg'%x)
-        #buz.on()
-        #led_red.on()
-        #buz.off()
-        #sleep(2)
-        #sleep(5)
-        #led_red.off()
+        display.lcd_display_string("Alarm #" + str(x), 2)
+        camera.capture('/home/guy/image%d.jpg' % x)
+        # buz.on()
+        # led_red.on()
+        # buz.off()
+        # sleep(2)
+        # sleep(5)
+        # led_red.off()
         sleep(3)
-    
+
     pir.wait_for_no_motion()
