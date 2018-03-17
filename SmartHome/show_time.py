8b9f7429124c0899e195ba5ff4cf1b5df66c53ca
guydvir@rpi2 <guydvir.tech@gmail.com>
2018-03-17 19:08:23 +0200

    GUY

--- SmartHome/show_time.py
+++ SmartHome/show_time.py
@@ -4,14 +4,16 @@ from time import sleep
 
 sys.path.append('/home/guy/Documents/github/Rpi/GPIO_Projects/lcd')
 import use_lcd
+
 lcd=use_lcd.MyLCD()
 now = str(datetime.datetime.now())
 print(now)
 
 try:
     while True:
-        now = str(datetime.datetime.now())[:-5].split(' ')
+        now = str(datetime.datetime.now())[:-7].split(' ')
         lcd.center_str(text1=now[0], text2=now[1])
+        sleep(0.5)
 except KeyboardInterrupt:
     print('End by user,',now[0],now[1])
     lcd.clear_lcd()
