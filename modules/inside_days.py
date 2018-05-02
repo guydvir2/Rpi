266ed6cc43a914ab26cd4f51a03b2619e3439317
guy dvir <guydvir2@gmail.com>
2018-05-02 08:38:39 +0300

    jiji

--- modules/inside_days.py
+++ modules/inside_days.py
@@ -5,20 +5,21 @@ today_day = now.isoweekday()
 today_hour = now.time()
 clock_format = '%H:%M:%S'
 
-start_day = 2
-end_day = 2
-datetime.datetime.strptime('12:00:00', clock_format)
+# print(datetime.datetime.isocalendar())
+days = [None, 7, 1, 2, 3, 4, 5, 6]
+start_day = 1
+end_day = 6
 start_hour = datetime.datetime.strptime('12:00:00', clock_format)
 end_hour = datetime.datetime.strptime('17:00:00', clock_format)
 
-if end_day - start_day < 0:
-    end_day_corrected = end_day + 7
+if days[end_day] - today_day < 0 and days[start_day] - today_day < 0:
+    end_day_corrected = days[end_day] + 7
 else:
     end_day_corrected = end_day
 
 start_datetime = datetime.datetime.combine(
-    datetime.datetime.now().date() + datetime.timedelta(days=start_day - today_day), start_hour.time())
+    datetime.datetime.now().date() + datetime.timedelta(days=days[start_day] - today_day), start_hour.time())
 end_datetime = datetime.datetime.combine(
-    datetime.datetime.now().date() + datetime.timedelta(days=end_day_corrected - today_day), end_hour.time())
+    datetime.datetime.now().date() + datetime.timedelta(days=days[end_day] - today_day), end_hour.time())
 
 print(start_datetime, end_datetime)
