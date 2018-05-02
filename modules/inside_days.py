299d25516b153c0e06b4b4393cb74b6cdfd488d7
Guy Dvir <guydvir2@gmail.com>
2018-05-02 14:12:37 +0300

    fixing days interval2

--- modules/inside_days.py
+++ modules/inside_days.py
@@ -1,94 +1,54 @@
 import datetime
 
 
-def day_shift_time_tuple(delta_days, time):
-    """create a datetime tuple of shifted day- days and clock"""
-    clock_format = '%H:%M:%S'
-    clock = datetime.datetime.strptime(time, clock_format)
+class WeekTimeInterval:
+    def __init__(self, day_start, hour_start, day_end, hour_end):
+        self.day_start = day_start
+        self.day_end = day_end
+        self.hour_start = hour_start
+        self.hour_end = hour_end
 
-    shifted_datetime = datetime.datetime.combine(
-        datetime.datetime.now().date() + datetime.timedelta(days=delta_days), clock.time())
-    return shifted_datetime
+    def day_shift_time_tuple(self, delta_days, time):
+        """create a datetime tuple of shifted day- days and clock"""
+        clock_format = '%H:%M:%S'
+        clock = datetime.datetime.strptime(time, clock_format)
 
+        shifted_datetime = datetime.datetime.combine(
+            datetime.datetime.now().date() + datetime.timedelta(days=delta_days), clock.time())
+        return shifted_datetime
 
-def shift_from_toady_time_tuple(day, hour):
-    shifted_datetime = day_shift_time_tuple(day - iso2h_day_convert(datetime.datetime.now().isoweekday()), hour)
-    return shifted_datetime
+    def shift_from_toady_time_tuple(self, day, hour):
+        shifted_datetime = self.day_shift_time_tuple(day - self.iso2h_day_convert(datetime.datetime.now().isoweekday()),
+                                                     hour)
+        return shifted_datetime
 
+    def iso2h_day_convert(self, iso_day):
+        if 1 <= iso_day <= 6:
+            day = iso_day + 1
+        elif iso_day == 7:
+            day = 1
+        else:
+            day = None
+        return day
 
-def iso2h_day_convert(iso_day):
-    if 1 <= iso_day <= 6:
-        day = iso_day + 1
-    elif iso_day == 7:
-        day = 1
-    else:
-        day = None
-    return day
+    def h2iso_convert_day(self, ay):
+        if 2 <= day <= 7:
+            iso_day = day - 1
+        elif day == 1:
+            iso_day = 7
+        return iso_day
 
+    def get_datetimes(self):
+        start_datetime = self.shift_from_toady_time_tuple(self.day_start, self.hour_start)
+        if self.day_end - self.day_start >= 0:
+            end_datetime = self.shift_from_toady_time_tuple(self.day_end, self.hour_end)
+        else:
+            end_datetime = self.shift_from_toady_time_tuple(self.day_end + 7, self.hour_end)
 
-def h2iso_convert_day(day):
-    if 2 <= day <= 7:
-        iso_day = day - 1
-    elif day == 1:
-        iso_day = 7
-    return iso_day
+        if start_datetime > end_datetime:
+            end_datetime += datetime.timedelta(days=7)
+        return start_datetime, end_datetime
 
-
-# def delta_days(iso_day1, iso_day2):
-#     delt_d = iso2h_day_convert(iso_day2) - iso2h_day_convert(iso_day1)
-#     if delt_d < 0:
-#         delt_d += 7
-#     return delt_d
-
-
-# print(delta_days(1,3))
-
-# now = datetime.datetime.now()
-# today_day = iso2h_day_convert(now.isoweekday())
-# today_hour = now.time()
-# clock_format = '%H:%M:%S'
-
-start_day = 7
-end_day = 6
-start_hour = "13:00:10"
-end_hour = "11:00:00"
-
-# start_hour = datetime.datetime.strptime('12:00:00', clock_format)
-# end_hour = datetime.datetime.strptime('17:00:00', clock_format)
-
-# print(day_shift_time_tuple(2, '14:00:00'))
-
-# scenario A
-start_datetime = shift_from_toady_time_tuple(start_day, start_hour)
-if end_day - start_day >= 0:
-    # start_datetime = shift_from_toady_time_tuple(start_day, start_hour)
-    end_datetime = shift_from_toady_time_tuple(end_day, end_hour)
-else:
-    end_datetime = shift_from_toady_time_tuple(end_day + 7, end_hour)
-
-if start_datetime > end_datetime:
-    end_datetime +=datetime.timedelta(days=7)
-    print("BAAAA")
-    #
-    # if start_day >= today_day:
-    #     start_datetime = shift_from_toady_time_tuple(start_day, start_hour)
-    #     end_datetime = shift_from_toady_time_tuple(end_day+7, end_hour)
-    # elif start_day < today_day:
-    #     start_datetime = shift_from_toady_time_tuple(start_day, start_hour)
-    #     end_datetime = shift_from_toady_time_tuple(end_day + 7, end_hour)
-
-print(start_datetime, end_datetime)
-
-#
-#
-# if days[end_day] - today_day < 0 and days[start_day] - today_day < 0:
-#     end_day_corrected = days[end_day] + 7
-# else:
-#     end_day_corrected = end_day
-#
-# start_datetime = datetime.datetime.combine(
-#     datetime.datetime.now().date() + datetime.timedelta(days=days[start_day] - today_day), start_hour.time())
-# end_datetime = datetime.datetime.combine(
-#     datetime.datetime.now().date() + datetime.timedelta(days=days[end_day] - today_day), end_hour.time())
-#
-# print(start_datetime, end_datetime)
+if __name__ == "__main__":
+    a = WeekTimeInterval(5, "14:00:00", 4, "12:00:00").get_datetimes()
+    print(a)
--- modules/wifi_shut.py
+++ modules/wifi_shut.py
@@ -5,6 +5,55 @@ from sys import platform
 import datetime
 
 
+class WeekTimeInterval:
+    def __init__(self, day_start, hour_start, day_end, hour_end):
+        self.day_start = day_start
+        self.day_end = day_end
+        self.hour_start = hour_start
+        self.hour_end = hour_end
+
+    def day_shift_time_tuple(self, delta_days, time):
+        """create a datetime tuple of shifted day- days and clock"""
+        clock_format = '%H:%M:%S'
+        clock = datetime.datetime.strptime(time, clock_format)
+
+        shifted_datetime = datetime.datetime.combine(
+            datetime.datetime.now().date() + datetime.timedelta(days=delta_days), clock.time())
+        return shifted_datetime
+
+    def shift_from_toady_time_tuple(self, day, hour):
+        shifted_datetime = self.day_shift_time_tuple(day - self.iso2h_day_convert(datetime.datetime.now().isoweekday()),
+                                                     hour)
+        return shifted_datetime
+
+    def iso2h_day_convert(self, iso_day):
+        if 1 <= iso_day <= 6:
+            day = iso_day + 1
+        elif iso_day == 7:
+            day = 1
+        else:
+            day = None
+        return day
+
+    def h2iso_convert_day(self, ay):
+        if 2 <= day <= 7:
+            iso_day = day - 1
+        elif day == 1:
+            iso_day = 7
+        return iso_day
+
+    def get_datetimes(self):
+        start_datetime = self.shift_from_toady_time_tuple(self.day_start, self.hour_start)
+        if self.day_end - self.day_start >= 0:
+            end_datetime = self.shift_from_toady_time_tuple(self.day_end, self.hour_end)
+        else:
+            end_datetime = self.shift_from_toady_time_tuple(self.day_end + 7, self.hour_end)
+
+        if start_datetime > end_datetime:
+            end_datetime += datetime.timedelta(days=7)
+        return start_datetime, end_datetime
+
+
 class Time2Start:
     def __init__(self):
         self.tasks_status, self.weekly_tasks_list = [], []
@@ -16,40 +65,52 @@ class Time2Start:
     def swipe_weekly_schedule(self, wtask):
         task_output_result = []
         # for loop for multiple days in task
+
         for i, day_task_start in enumerate(wtask['start_days']):
             status_dict = {}
-            # self.tasks_status[-1].append([])
             day_task_end = wtask['end_days'][i]
-            timenow = self.time_now()
-            delta_time_start = datetime.datetime.combine(timenow.date(),
-                                                         datetime.datetime.strptime(wtask['start_time'],
-                                                                                    self.clock_format).time()) - timenow
-            delta_time_end = datetime.datetime.combine(timenow.date(),
-                                                       datetime.datetime.strptime(wtask['end_time'],
-                                                                                  self.clock_format).time()) - datetime.datetime.combine(
-                timenow.date(),
-                datetime.datetime.strptime(wtask['start_time'], self.clock_format).time())
+            status_dict['start'], status_dict['end'] = WeekTimeInterval(day_start=day_task_start,
+                                                                        hour_start=wtask['start_time'],
+                                                                        day_end=day_task_end,
+                                                                        hour_end=wtask['end_time']).get_datetimes()
+            if status_dict['start'] <= datetime.datetime.now() <= status_dict['end']:
+                status_dict['state'] = 1
 
-            delta_days_start = day_task_start - timenow.isoweekday()
-            if delta_days_start < 0:
-                delta_days_start += 7
-            time_to_start = datetime.timedelta(days=delta_days_start) + delta_time_start
-
-            delta_days_end = day_task_end - day_task_start
-            if delta_days_end < 0:
-                delta_days_end += 7
-            time_to_end = datetime.timedelta(days=delta_days_end) + delta_time_end + time_to_start
-
-            status_dict['start'] = time_to_start
-            status_dict["end"] = time_to_end
-            if time_to_start.total_seconds() < 0:
-                if time_to_end.total_seconds() > 0:
-                    status_dict['state'] = 1
             else:
                 status_dict['state'] = 0
-            task_output_result.append(status_dict)
 
-        return task_output_result
+            #     timenow = self.time_now()
+            #     delta_time_start = datetime.datetime.combine(timenow.date(),
+            #                                                  datetime.datetime.strptime(wtask['start_time'],
+            #                                                                             self.clock_format).time()) - timenow
+            #     delta_time_end = datetime.datetime.combine(timenow.date(),
+            #                                                datetime.datetime.strptime(wtask['end_time'],
+            #                                                                           self.clock_format).time()) - datetime.datetime.combine(
+            #         timenow.date(),
+            #         datetime.datetime.strptime(wtask['start_time'], self.clock_format).time())
+            #
+            #     delta_days_start = day_task_start - timenow.isoweekday()
+            #     if delta_days_start < 0:
+            #         delta_days_start += 7
+            #     time_to_start = datetime.timedelta(days=delta_days_start) + delta_time_start
+            #
+            #     delta_days_end = day_task_end - day_task_start
+            #     if delta_days_end < 0:
+            #         delta_days_end += 7
+            #     time_to_end = datetime.timedelta(days=delta_days_end) + delta_time_end + time_to_start
+            #
+            #     status_dict['start'] = time_to_start
+            #     status_dict["end"] = time_to_end
+            #     if time_to_start.total_seconds() < 0:
+            #         if time_to_end.total_seconds() > 0:
+            #             status_dict['state'] = 1
+            #     else:
+            #         status_dict['state'] = 0
+            #     task_output_result.append(status_dict)
+            #
+            # return task_output_result
+            task_output_result.append(status_dict)
+            print(task_output_result)
 
     def exec_tasks_run(self):
         self.tasks_status = [[] for i in range(len(self.weekly_tasks_list))]
@@ -77,7 +138,7 @@ class Time2Start:
         def inject_tasks_to_schedule():
             self.tasks_status = list(map(self.swipe_weekly_schedule, self.weekly_tasks_list))
             print(self.tasks_status)
-            is_any_task_on()
+            # is_any_task_on()
             # print(is_task_engaged_and_on())
 
             # print_task_status = lambda status, i=0: print(
@@ -154,7 +215,7 @@ class WifiControl:
 # a.wifi_on()
 
 b = Time2Start()
-b.add_weekly_task(new_task={'start_days': [1, 4], 'start_time': '11:30:00', 'end_days': [5, 3], 'end_time': '22:00:00'})
-# b.add_weekly_task(new_task={'start_days': [3], 'start_time': '07:30:00', 'end_days': [7], 'end_time': '22:35:00'})
+b.add_weekly_task(new_task={'start_days': [1, 4], 'start_time': '11:30:00', 'end_days': [5, 6], 'end_time': '22:00:00'})
+b.add_weekly_task(new_task={'start_days': [3], 'start_time': '07:30:00', 'end_days': [3], 'end_time': '22:35:00'})
 #
 b.exec_tasks_run()
