158860d6bd5b2d9a74bedca851e19c39df392152
Guy Dvir <guydvir2@gmail.com>
2018-05-02 07:47:43 +0300

    fixing days interval

--- modules/inside_days.py
+++ modules/inside_days.py
@@ -3,18 +3,22 @@ import datetime
 now = datetime.datetime.now()
 today_day = now.isoweekday()
 today_hour = now.time()
+clock_format = '%H:%M:%S'
 
-start_day = 3
-end_day = 0
-start_hour = now.time()
-end_hour = now.time()
+start_day = 2
+end_day = 2
+datetime.datetime.strptime('12:00:00', clock_format)
+start_hour = datetime.datetime.strptime('12:00:00', clock_format)
+end_hour = datetime.datetime.strptime('17:00:00', clock_format)
 
 if end_day - start_day < 0:
     end_day_corrected = end_day + 7
 else:
     end_day_corrected = end_day
 
-start_datetime = now + datetime.timedelta(days=start_day - today_day)
-end_datetime = now + datetime.timedelta(days=end_day_corrected - today_day)
+start_datetime = datetime.datetime.combine(
+    datetime.datetime.now().date() + datetime.timedelta(days=start_day - today_day), start_hour.time())
+end_datetime = datetime.datetime.combine(
+    datetime.datetime.now().date() + datetime.timedelta(days=end_day_corrected - today_day), end_hour.time())
 
 print(start_datetime, end_datetime)
