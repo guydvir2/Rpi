537f1d25a44a92c9e08c065d49ef7b601cf84452
guy dvir <guydvir2@gmail.com>
2018-05-02 12:46:46 +0300

    sfsd

--- modules/inside_days.py
+++ modules/inside_days.py
@@ -1,26 +1,22 @@
 import datetime
 
 
-def create_datetime(delta_days, time):
+def day_shift_time_tuple(delta_days, time):
+    """create a datetime tuple of shifted day- days and clock"""
     clock_format = '%H:%M:%S'
-    # today_day = datetime.datetime.now().isoweekday()
     clock = datetime.datetime.strptime(time, clock_format)
-    start_datetime = datetime.datetime.combine(
+
+    shifted_datetime = datetime.datetime.combine(
         datetime.datetime.now().date() + datetime.timedelta(days=delta_days), clock.time())
-    return start_datetime
+    return shifted_datetime
 
 
-def decide_dates(day, hour):
-    today_day = convert_iso_days(now.isoweekday())
-    delt_days = day - today_day
-    if delt_days < 0:
-        start_datetime = create_datetime(delt_days, hour)
-    else:
-        start_datetime = create_datetime(-delt_days, hour)
-    return start_datetime
+def shift_from_toady_time_tuple(day, hour):
+    shifted_datetime = day_shift_time_tuple(day - iso2h_day_convert(datetime.datetime.now().isoweekday()), hour)
+    return shifted_datetime
 
 
-def convert_iso_days(iso_day):
+def iso2h_day_convert(iso_day):
     if 1 <= iso_day <= 6:
         day = iso_day + 1
     elif iso_day == 7:
@@ -30,7 +26,7 @@ def convert_iso_days(iso_day):
     return day
 
 
-def convert_toiso_day(day):
+def h2iso_convert_day(day):
     if 2 <= day <= 7:
         iso_day = day - 1
     elif day == 1:
@@ -38,38 +34,50 @@ def convert_toiso_day(day):
     return iso_day
 
 
-def delta_days(iso_day1, iso_day2):
-    delt_d = convert_iso_days(iso_day2) - convert_iso_days(iso_day1)
-    if delt_d < 0:
-        delt_d += 7
-    return delt_d
+# def delta_days(iso_day1, iso_day2):
+#     delt_d = iso2h_day_convert(iso_day2) - iso2h_day_convert(iso_day1)
+#     if delt_d < 0:
+#         delt_d += 7
+#     return delt_d
 
 
 # print(delta_days(1,3))
 
-# print(datetime.datetime.isocalendar())
+# now = datetime.datetime.now()
+# today_day = iso2h_day_convert(now.isoweekday())
+# today_hour = now.time()
+# clock_format = '%H:%M:%S'
 
-now = datetime.datetime.now()
-today_day = convert_iso_days(now.isoweekday())
-today_hour = now.time()
-clock_format = '%H:%M:%S'
-
-start_day = 1
+start_day = 7
 end_day = 6
-start_hour = "12:00:10"
-end_hour = "17:00:00"
+start_hour = "13:00:10"
+end_hour = "11:00:00"
 
 # start_hour = datetime.datetime.strptime('12:00:00', clock_format)
 # end_hour = datetime.datetime.strptime('17:00:00', clock_format)
 
-# print(create_datetime(2, '14:00:00'))
+# print(day_shift_time_tuple(2, '14:00:00'))
 
 # scenario A
-if end_day - start_day > 0:
-    start_datetime = decide_dates(start_day, start_hour)
-    end_datetime = decide_dates(end_day, end_hour)
+start_datetime = shift_from_toady_time_tuple(start_day, start_hour)
+if end_day - start_day >= 0:
+    # start_datetime = shift_from_toady_time_tuple(start_day, start_hour)
+    end_datetime = shift_from_toady_time_tuple(end_day, end_hour)
+else:
+    end_datetime = shift_from_toady_time_tuple(end_day + 7, end_hour)
 
-    print(start_datetime, end_datetime)
+if start_datetime > end_datetime:
+    end_datetime +=datetime.timedelta(days=7)
+    print("BAAAA")
+    #
+    # if start_day >= today_day:
+    #     start_datetime = shift_from_toady_time_tuple(start_day, start_hour)
+    #     end_datetime = shift_from_toady_time_tuple(end_day+7, end_hour)
+    # elif start_day < today_day:
+    #     start_datetime = shift_from_toady_time_tuple(start_day, start_hour)
+    #     end_datetime = shift_from_toady_time_tuple(end_day + 7, end_hour)
+
+print(start_datetime, end_datetime)
 
 #
 #
