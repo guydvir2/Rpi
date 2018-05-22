d4ee71daac49a441916b0ca9b45cc66fcee245d7
GuyUSH <guydvir.tech@gmail.com>
2018-05-22 21:55:04 +0300

    logic is OK

--- GPIO_Projects/pressbutton.py
+++ GPIO_Projects/pressbutton.py
@@ -1,31 +1,64 @@
+import datetime
+import gpiozero
+from signal import pause
 import time
 
 
 class MultiPressButton:
-    def __init__(self, master, status):
-        self.press_time, self.released_time = None, None
-        self.status, self.command, self.master = status, None, master
+    def __init__(self, master):
+        self.press_time, self.released_time , self.last_released_time, self.end_press= datetime.timedelta(0), datetime.timedelta(0), datetime.timedelta(0), datetime.timedelta(0)
+        self.status, self.command, self.master = None, None, master
+        self.counter, self.state =0, 0
+        self.button_monitor()
 
     def button_monitor(self):
+        t_int=0.2
         while True:
-            if self.status is True:
-                start_press_time = time.time()
-                while self.status is True:
-                    self.press_time = time.time() - start_press_time
-                print(self.press_time)
-            elif self.status is False:
-                start_released_time = time.time()
-                while self.status is False:
-                    self.released_time = time.time() - start_released_time
-                print(self.released_time)
-
-            return self.press_time, self.released_time
+            self.released_time =datetime.datetime.now() - self.end_press
+            if self.master.value is True:
+                start_press_time = datetime.datetime.now()
+                while self.master.value is True:
+                    t_now= datetime.datetime.now()
+                    time.sleep(t_int/2)
+                    self.press_time = t_now - start_press_time
+                    
+                print("press duration:", self.press_time.total_seconds())
+                self.end_press = t_now
+                self.press_conditions()
 
     def press_conditions(self):
-        if self.press_time > 3:
-            self.command, self.press_time, self.released_time = 0, 0, 0
-        if self.released_time > 3:
-            self.command, self.press_time, self.released_time = 0, 0, 0
+        try:
+            self.released_time.total_seconds()
+        except AttributeError:
+            self.released_time = datetime.timedelta(seconds=0.01)
 
-        if self.press_time < 0.5 and self.released_time < 2:
-            self.command, self.press_time, self.released_time = 1, 0, 0
+        print(self.released_time.total_seconds())
+        # Reset
+        if self.press_time.total_seconds() > 4:
+            self.command = 0#, datetime.timedelta(0), datetime.timedelta(0)
+            print(self.command)
+            
+        #standard press
+        elif self.press_time.total_seconds() < 0.5:
+            if self.command == 2 and self.released_time.total_seconds() < 3:
+                self.counter +=1
+                print("counter is:",self.counter)
+                self.released_time = datetime.timedelta(0)
+            else:
+                self.command, self.counter = 1,0
+                print(self.command)
+
+        # Enter program Mode
+        elif 1< self.press_time.total_seconds() < 2.7:
+            self.command = 2
+            print(self.command)
+            
+def print_hi():
+    print("hi")
+    
+button = gpiozero.Button(20)
+a=MultiPressButton(button)
+
+print("KAKI")
+
+pause()
